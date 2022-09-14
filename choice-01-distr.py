8559901c143ad140213b23cf7ba0c79e34a38453
Serj <serj@localhost>
2022-09-14 09:06:24 +0300

    choice global loop

--- choice-01-distr.py
+++ choice-01-distr.py
@@ -1,7 +1,14 @@
 #! /usr/bin/env python3
 from random import choice
+global_trials=5
 trials=1000
 opts=["cin", "mate", "macos"]
-lst=[choice(opts) for _ in range (opts)]
-res=[(opt, lst.count(opts)) for opt in opts]
-print (res)
+globres=dict([(opt, 0) for opt in opts])
+for i in range (global_trials):
+    lst=[choice(opts) for _ in range (trials)]
+    res=[(opt, lst.count(opt)) for opt in opts]
+    print (res)
+    resel=max(res, key=lambda e: e[1])
+    print ("res="+str(resel))
+    globres[resel[0]]+=1
+print (globres)
