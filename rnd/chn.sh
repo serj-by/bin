eea5c5caa8f7106c0b48c63d8595b594d4f2280b
Serj <serj.by@yahoo.com>
2022-10-09 11:14:30 +0300

    Max result for rnd/chn.sh

--- rnd/chn.sh
+++ rnd/chn.sh
@@ -1,9 +1,16 @@
 #! /usr/bin/env bash
 # credits: https://stackoverflow.com/questions/9057387/process-all-arguments-except-the-first-one-in-a-bash-script (via Pavman)
-echo "times: $1"
-echo "opts: ${@:2}"
+#	   https://stackoverflow.com/questions/43440425/most-frequent-element-in-an-array-bash-3-2 via rici
+#echo "times: $1"
+#echo "opts: ${@:2}"
+declare -a resarr=()
 for i in $(seq $1); do
 #echo $i
 #echo "exec ~/ch.py ${@:2}";
-~/dev/bin/rnd/ch.py ${@:2};
+res=`~/dev/bin/rnd/ch.py ${@:2}`
+echo "$res"
+resarr+=($res)
 done
+echo "ResArr: ${resarr[@]}"
+echo -n "MAX: "
+printf '%s\n' "${resarr[@]}" | sort -n | uniq -c | sort -k1,1nr -k2n | awk '{print $2; exit}'
