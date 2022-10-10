2d040649c7e3ab9b23c768dc9b2cacb3e1905b21
Serj <serj.by@yahoo.com>
2022-10-11 01:45:07 +0300

    Parameter processing for addpath

--- addpath.sh
+++ addpath.sh
@@ -1,4 +1,9 @@
+if [ -z $1 ]
+then
 NEWCMD="export PATH=\"`pwd`:\$PATH\""
+else
+NEWCMD="export PATH=\"`realpath $1`:\$PATH\""
+fi
 echo "Path cmd: $NEWCMD"
 echo "System: `uname -s`"
 if [[ `uname -s` -eq "Darwin" ]]
@@ -7,11 +12,12 @@ then
 else
  PROFILE="$HOME/.bashrc";
 fi
+echo "Profile file: $PROFILE"
 if [ ! -f $PROFLE ]; then
 echo "$PROFILE file not exists. Please check correct naming in `realpath $0`. Exiting."
 exit -1
 else
-echo "$PROFILE file exists. Adding $NEWCMD there."
+echo "$PROFILE file exists. Adding new path there."
 fi
 echo $NEWCMD >> $PROFILE
 echo "Re-applying profile ..."
