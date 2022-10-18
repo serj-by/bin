ae7730e5ee163689913c78e3ac14a26f3de167d3
Serj <serj.by@yahoo.com>
2022-10-18 21:02:42 +0300

    MySQL module finally works!

--- initconfs_linux/CREDITS.md
+++ initconfs_linux/CREDITS.md
@@ -5,3 +5,4 @@ https://www.mybluelinux.com/bash-guide-to-bash-arrays/#bash_array_contains_a_val
 https://linuxize.com/post/how-to-check-if-string-contains-substring-in-bash/
 https://stackoverflow.com/questions/2264428/how-to-convert-a-string-to-lower-case-in-bash
 https://linuxcommand.org/lc3_man_pages/tr1.html
+https://linux.die.net/man/1/grep
--- initconfs_linux/modules/mysql.sh
+++ initconfs_linux/modules/mysql.sh
@@ -2,6 +2,8 @@
 #! /usr/bin/env bash
 actions="install-wb"
 
+SKIP_DOWNLOAD="true"
+
 if [[ $actions == *"install-my"* ]]; then
 MYSQLURL="https://dev.mysql.com/"
 INITPART="downloads/repo/apt/"
@@ -34,13 +36,13 @@ fi
 
 # Snap version of Workbench looks like broken so installing it from dev site
 # install-wb
+if [[ $actions == *"install-wb"* ]]; then
 # Workench download configs
 WBINI_TPL="https://downloads.mysql.com/archives/workbench/?tpl=version&os=22&version=%VER%&osva="
-WB_INITFN="/tmp/wbinit.hml"
-if [[ $actions == *"install-wb"* ]]; then
+WBDEB_INITFN="/tmp/wbdebinit.html"
 UPSTREAM_OS=`cat /etc/upstream-release/lsb-release | grep -Po '(?<=DISTRIB_ID=)(.+)$'`
 UPSTREAM_VER=`cat /etc/upstream-release/lsb-release | grep -Po '(?<=DISTRIB_RELEASE=)([\d\.]+)$'`
-echo "Upstream OS: $UPSTREAM_OS version: $UPSTREAM_VER"
+echo "Upstream OS: $UPSTREAM_OS version $UPSTREAM_VER"
 WB_VER="-"
 if [ $UPSTREAM_OS = "Ubuntu" ]; then
   case $UPSTREAM_VER in
@@ -59,24 +61,37 @@ if [ "$WB_VER" = "-" ]; then
 else
   echo "For your upstream OS version ($UPSTREAM_VER) most suitale wookbench version is $WB_VER"
 fi
-
 WBDEB_BASEURL="https://downloads.mysql.com/"
-WBINI_URL=`echo "$WBINI_TPL" | sed "s@%VER%@$WB_VER@"`
+WBINIT_URL=`echo "$WBINI_TPL" | sed "s@%VER%@$WB_VER@"`
+if [ -z "$SKIP_DOWNLOAD" ]; then
 echo "Processing to download..."
+wget $WBINIT_URL -O $WBDEB_INITFN
+else
+echo "SKIP_DOWNLOAD flag set to $SKIP_DOWNLOAD. Download skipping..."
+fi
 #gopen $WB_INITFN
-wget $WBINI_URL -O $WB_INITFN
 WBDEB_REGEXP_ARCHPART="_amd\d+"
 WBDEB_REGEXP_VERSPART="`echo $UPSTREAM_OS | tr '[:upper:]' '[:lower:]'`$UPSTREAM_VER$WBDEB_REGEXP_ARCHPART"
 WBDEB_REGEXP="\/archives\/get\/p\/\d*\/file\/mysql\-workbench\-community_[\d\.\-]+$WBDEB_REGEXP_VERSPART\.deb"
-WBDEB_RELURL=`cat $WB_INITFN | grep -oP '${WBDEB_REGEXP}'`
+WBDEB_RELURLCMD="grep -oP '${WBDEB_REGEXP}' $WBDEB_INITFN"
+#WBDEB_RELURLCMD="echo /archives/get/p/8/file/mysql-workbench-community_8.0.21-1ubuntu20.04_amd64.deb"
+#echo "Try 2 exec $WBDEB_RELURLCMD !!!*"
+WBDEB_RELURL=`eval "$WBDEB_RELURLCMD"`
 WBDEB_FN=`echo -n $WBDEB_RELURL | grep -oP 'mysql\-workbench\-community_[\d\.]+\-.+?\.deb'`
+WBDEB_PATH="/tmp/$WBDEB_FN"
 WBDEB_URL="$WBDEB_BASEURL$WBDEB_RELURL"
+
 echo "WB .deb ver part RegExp: $WBDEB_REGEXP_VERSPART"
 echo "WB .deb regexp: $WBDEB_REGEXP"
-echo "WB .deb init URL: $WBINI_URL"
+echo "WB .deb init URL: $WBINIT_URL"
+echo ".deb rel cmd: $WBDEB_RELURLCMD"
 echo ".deb rel URL: $WBDEB_RELURL"
 echo ".deb URL: $WBDEB_URL"
 echo ".deb FN: $WBDEB_FN"
+echo ".deb path: $WBDEB_PATH"
+
+wget -O $WBDEB_PATH -- $WBDEB_URL && echo "WB .deb downloaded. Installing..."
+sudo dpkg -i $WBDEB_PATH && echo "MySQL WB installed."
 
 fi
 # /install-wb
