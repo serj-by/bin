29d76bf7e1e4ac1d907ad372fb5ccec658b0b3ee
Serj <serj.by@yahoo.com>
2022-11-12 20:56:14 +0300

    ssymlink resolution

--- filtersections.py
+++ filtersections.py
@@ -1,1 +1,22 @@
-/Users/serj/www/locamp/reiki_ru/r/extensions/Synthes/sql/filtersections.py
+#! /usr/bin/en python3
+import re, sys
+
+with open (sys.argv[1]) as f:
+	t = f.read ()
+
+
+
+
+
+
+sections=re.findall("--sections=([\w\d|]+)$", sys.argv[1])
+print ("arg", sys.argv[2])
+print ("si", sections)
+if sections is not None:
+	sections=[s[0] for s in sections]
+	m = re.findall("--\s+{ @myimport-section ("+sections+") }\s+([^-]+)\s+--", t, re.MULTILINE)
+	res="\n".join ([t[1] for t in m])
+	print (m, res)
+else:
+	print ("-- no param --sections pecified. Using whole file.")
+	print (t)
