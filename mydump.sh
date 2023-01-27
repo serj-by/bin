8d951bff3c29a1eeabf5f0ecb2c06bc3c314bf59
Serj <serj.by@yahoo.com>
2023-01-27 19:20:58 +0300

    backup works with views

--- mydump.sh
+++ mydump.sh
@@ -12,4 +12,7 @@ if [ -n "$2" ]; then
 cmnt=`echo "$2" | sed "s@[^a-zA-Z0-9ЁёА-я]@_@g"`
 echo "Comment part of file name: $cmnt"
 fi
+echo "Dumping main SQL data..."
 mysqldump --login-path=$dbloginpath --verbose $1 > $1_`sbdate`__`sbtime short_`__$cmnt.sql
+echo "Dumping SQL views..."
+myq.sh --dumpviews --silent > views_$1_`sbdate`__`sbtime short_`__$cmnt.sql
