30e8c20b41efcacbb3c3be449646c8a05448096f
Serj <serj.by@yahoo.com>
2023-01-30 23:39:12 +0300

    sbdate fix for cases when bad numbers form between dat components

--- mydump.sh
+++ mydump.sh
@@ -13,6 +13,6 @@ cmnt=`echo "$2" | sed "s@[^a-zA-Z0-9ЁёА-я]@_@g"`
 echo "Comment part of file name: $cmnt"
 fi
 echo "Dumping main SQL data..."
-mysqldump --login-path=$dbloginpath --verbose $1 > $1_`sbdate`__`sbtime short_`__$cmnt.sql
+mysqldump --login-path=$dbloginpath --verbose --routines $1 > $1_`sbdate`__`sbtime short_`__$cmnt.sql
 echo "Dumping SQL views..."
 myq.sh --dumpviews --silent > views_$1_`sbdate`__`sbtime short_`__$cmnt.sql
