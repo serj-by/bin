2f9472149f6afaf57d2e74509a62cba370c76361
Serj <serj.by@yahoo.com>
2023-03-12 09:53:56 +0300

    stparse works well now

--- stpaste.sh
+++ stpaste.sh
@@ -1,10 +1,17 @@
 #! /usr/bin/env bash
 USAGE="
 Usage:\n\
-$0 <dump_suffix>>
+$0 [dump_suffix]
 "
-if [ -z $1 ]; then
-echo -e $USAGE;
-exit -1;
+dumpSfx="partial_wnames";
+if [ -n "$1" ]; then
+dumpSfx="$1";
+echo "$dumpSfx passed as suffix";
+  if [ "$1" = "--help" ]; then
+  echo -e $USAGE;
+  exit -1;
+  fi
 fi
-pbpaste > dump_`sbtime short_`_$1.csv
+fn="dump_`sbtime short_`_$dumpSfx.csv"
+echo "Filename of dump: $fn";
+pbpaste > $fn
