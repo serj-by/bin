cb1f6766a9d27e437621bdbe8e7f6b405ab45a96
Serj <serj.by@yahoo.com>
2023-03-12 06:45:55 +0300

    stpastemig.sh

--- stpastemig.sh
+++ stpastemig.sh
@@ -6,14 +6,15 @@ $0 [migration_name]
 . cdmig
 migpath=`pwd`/
 migfn="synthes_mig__`sbdate`__`sbtime short_`.sql"
-echo "Migfn: $migfn"
-exit 0
 echo "Mig path: $migpath"
-if [[ $1 == "--help" ]]; then
+echo "Mig fn: $migfn"
+if [ "$1" == "--help" ]; then
 echo -e $USAGE;
 exit 0;
-elif [ -nz $1 ]; then
-migfn=$1;
+elif [[ -n $1 ]]; then
+migfn=synthes_mig__$1;
 fi
-echo cp $mipath_template.sql $migfn
-echo pbpaste >> $migfn
+cd $migpath
+#cp _template.sql $migfn
+sed "s/{{date}}/`sbdate dashed`/g" _template.sql | sed "s/{{time}}/`sbtime`/g"> $migfn
+pbpaste >> $migfn
--- stseedcsv.sh
+++ stseedcsv.sh
@@ -1,2 +1,2 @@
-. cdseedlog
-php /Users/serj/www/locamp/reiki_ru/r/extensions/Synthes/maintenance/seedfromcsv.php | tee seedfromcsv_log_`sbtime short_`.txt
+. cdseedlogs
+php /Users/serj/www/locamp/reiki_ru/r/extensions/Synthes/maintenance/seedfromcsv.php | tee seedfromcsv_log__`sbdate`__`sbtime short_`.txt
