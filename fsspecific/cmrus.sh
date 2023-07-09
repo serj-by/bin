cd768105c6bf516822a9016d0bd0a54f8e571427
serj-by <serj-by@users.noreply.github.com>
2023-07-09 15:06:14 +0300

    @cmrus.sh: autoupdate RuSlova on 230709 @ 15:05_01:14

--- fsspecific/cmrus.sh
+++ fsspecific/cmrus.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 . cdrus
 echo "@cmrus.sh: RuSlova dir: `pwd`"
-gcm_noint "@cmrus.sh: autoupdate RuSlova on `sbdate` @ `sbtime`"
+gcm "@cmrus.sh: autoupdate RuSlova on `sbdate` @ `sbtime`"
