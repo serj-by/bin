fd68686a63d87987fde182eaaa9b85bc99f36462
serj-by <serj-by@users.noreply.github.com>
2024-04-15 08:58:00 +0300

    cdsistem symlink

---
+++ fsspecific/cdx.sh
@@ -0,0 +1,9 @@
+cd "$HOME/Documents/Personal 2212/x.unz/.x/.myx"
+pwd
+if [ -z $1 ]; then
+echo "Param is empty. Exiting..."
+else
+echo "Param is not empty. Trying to open..."
+open .
+fi
+echo "Done."
